78c7623789edc6f8b3092c349f891bccabb2b506
wilferson <wilfersona@gmail.com>
2016-11-09 00:12:26 -0300

    1212

--- www/js/main.js
+++ www/js/main.js
@@ -1,11 +1,9 @@
 var db = null;
+var username=null;
+var useruniquekey=null;
+var isActivated=false;
+var coordinates=null;
+var deviceIMEI=null;
 
-            document.addEventListener("deviceready", function(){
-            db = window.sqlitePlugin.openDatabase({name: "com.onlytechve.otgps.db"});
-            db.transaction(function(tx) {
-            tx.executeSql("CREATE TABLE IF NOT EXISTS config (name text primary key, data text)");
-            }, function(err){
-                    alert("Error inicializando la base de datos.");
-                        });
-                        
-    }, false);                        
+document.addEventListener("deviceready",main, false);              
+$.mobile.defaultPageTransition='slideup'; 
