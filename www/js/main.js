163f381fa224bf579bf0f0d43a3e1aae151c3467
wilferson <wilfersona@gmail.com>
2016-11-10 00:04:51 -0300

    1204

--- www/js/main.js
+++ www/js/main.js
@@ -4,16 +4,45 @@ var useruniquekey=null;
 var isActivated=false;
 var coordinates=null;
 var deviceIMEI=null;
-
+var numero_de_gps=null;
+var serial_de_acti=null;
+var database_ready=false;
 document.addEventListener("deviceready",main, false);              
 $.mobile.defaultPageTransition='slideup'; 
 
 function main()
 {
-$.ajax({
-    url : "/mysite/file.html",
-    success : function(result){
-        alert(result);
-    }
-});
+   
+
+}
+
+
+function register()
+{
+    serial_de_acti=document.getElementById("ikey").value;
+    numero_de_gps=document.getElementById("inumber").value
+    if (($.isNumeric(numero_de_gps))& (serial_de_acti.length>0))
+        {
+            $.ajax({
+            url : "http://app.carabobotrackers.com.ve/chekkey.php?key="+serial_de_acti+" &gpsnum="+numero_de_gps,
+            success : function(result){
+            
+                    switch (result){
+                        case "0":
+                            $(":mobile-pagecontainer").pagecontainer( "change", $("#welcome" ));
+                            break;
+                        case "1":
+                            alert("No se pudo conectar con la base de datos");
+                            break;
+                        case "2":
+                            alert("la clave introducida es invalida");
+                            break;
+                        case "3":
+                            alert("Ya existe un dispositivo registrado con esa clave");
+                            break;
+                    }
+
+    }});
+        }
+        else{alert("verifique los datos ingresados");}
 }
